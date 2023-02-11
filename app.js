0ee778a96c33c5ee97540a53738e7f7e339ecdf6
Kavindyap <kavindyaprr@gmail.com>
2023-02-11 15:23:42 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
+//Register a listener
+emitter.on('message logged', function(){
+    console.log('Listener called');
+});
+
+//Raise an event
+emitter.emit('messageLogged');
